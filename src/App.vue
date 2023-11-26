7988e65234d1b3a2cc4fc352ce3842f170c4e0a0
alyson.ramos <ramos.alysonfarias@gmail.com>
2023-11-26 18:53:47 -0300

    main menu

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+import MenuItem from '@/components/MenuItem.vue';
+
+import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
+import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
+import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+
+let openMenu = ref(false);
+</script>
+
+<template>
+  <div class="div">
+    <div class="w-[calc(100%-240px)]
+    h-[60px]
+    fixed
+    right-0
+    z-20
+    bg-[#101010]
+    bg-opacity-80
+    flex
+    items-center
+    justify-between
+    ">
+      <div class="flex items-center ml-6">
+        <button type="button" class="rounded-full bg-black p-[1px]">
+          <ChevronLeft fillColor="#FFFFFF" :size-="30"></ChevronLeft>
+        </button>
+        <button type="button" class="rounded-full bg-black p-[1px] ml-4">
+          <ChevronRight fillColor="#FFFFFF" :size-="30"></ChevronRight>
+        </button>
+      </div>
+      <button @click="openMenu = !openMenu" :class="openMenu ? 'bg-[#282828]' : 'bg-black'"
+        class="bg-black hover:bg-[#282828] rounded-full p-0.5 mr-8 mt-0.5 cursor-pointer">
+        <div class="flex items-center">
+          <img class="rounded-full" width="27"
+            src="https://media.licdn.com/dms/image/D4D03AQHY-ziWIcD0-A/profile-displayphoto-shrink_800_800/0/1696379423310?e=1706140800&v=beta&t=IwB_IVGK6UVgX6JGXbxJ6TvMZevqsmODJXz8SE3W6xc"
+            alt="user-profile">
+          <div class="text-white text-[14px] ml-1.5 font-semibold">Alyson Farias</div>
+          <ChevronDown v-if="!openMenu" fillColor="#FFFFFF" @click="openMenu = true" :size-="25"></ChevronDown>
+          <ChevronUp v-else="openMenu" fillColor="#FFFFFF" @click="openMenu = false" :size-="25"></ChevronUp>
+        </div>
+      </button>
+      <span v-if="openMenu"
+        class="fixed w-[190px] bg-[#282828] shadow-2xl z-50 rounded-sm top-[52px] right-[35px] p-1 cursor-pointer">
+        <ul class="text-gray-200 font-semibold text-[14px]">
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D] border-b border-b-gray-600">Profile</li>
+          <li class="px-3 py-2.5 hover:bg-[#3E3D3D]">Log out</li>
+        </ul>
+      </span>
+    </div>
+
+    <div id="SideNav" class="h-[100%] p-6 w-[240px] fixed z-50 bg-black">
+      <RouterLink to="/">
+        <img width="125" src="/images/icons/spotify-logo.png" alt="spotify-logo">
+      </RouterLink>
+      <div class="my-8"></div>
+      <ul>
+        <RouterLink to="/home">
+          <MenuItem class="ml-[1px]" :iconSize="23" name="Home" iconString="home" pageUrl="/home" />
+        </RouterLink>
+        <RouterLink to="/search">
+          <MenuItem class="ml-[1px]" :iconSize="23" name="Search" iconString="search" pageUrl="/search" />
+        </RouterLink>
+        <RouterLink to="/library">
+          <MenuItem class="ml-[1px]" :iconSize="23" name="Library" iconString="library" pageUrl="/library" />
+        </RouterLink>
+        <div class="py-3.5"></div>
+        <MenuItem :iconSize="24" name="Create Playlist" iconString="playlist" pageUrl="/playlist" />
+        <MenuItem class="-ml-[1px]" :iconSize="27" name="Liked" iconString="liked" pageUrl="/liked" />
+      </ul>
+      <div class="border-b border-b-gray-700"></div>
+      <ul>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #1</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #2</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #3</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #4</li>
+        <li class="font-semibold text-[13px] mt-3 text-gray-300 hover:text-white">My Playlist #5</li>
+      </ul>
+    </div>
+    <div class="fixed
+    right-0
+    top-0
+    w-[calc(100#-240px)]
+    overflow-auto
+    h-full
+    bg-gradient-to-b
+    from-[#1C1C1C]
+    to-black">
+      oi
+      <div class="mt-[70px]">
+        <RouterView />
+      </div>
+      <div class="mb-[100px]">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
