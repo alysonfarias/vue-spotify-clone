3f44afd2caa01fe675e652dd9ce5378354a8815a
alyson.ramos <ramos.alysonfarias@gmail.com>
2023-11-26 19:22:53 -0300

    feat: HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,60 @@
 <script setup lang="ts">
+import HomeCard from '@/components/HomeCard.vue';
 </script>
 
 <template>
-  <!-- <main>
-    hi
-  </main> -->
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Recommend podcasts
+    </button>
+    <div class="py-1 5"></div>
+    <div class="flex items-center">
+      <HomeCard image="https://picsum.photos/id/30/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard image="https://picsum.photos/id/31/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard class="md:block hidden" image="https://picsum.photos/id/32/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard class="lg:block hidden" image="https://picsum.photos/id/33/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard class="xl:block hidden" image="https://picsum.photos/id/35/300/300" title="Title here" subTitle="Subtitle here"/>
+    </div>
+  </div>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Recommend podcasts
+    </button>
+    <div class="py-1 5"></div>
+    <div class="flex items-center">
+      <HomeCard image="https://picsum.photos/id/30/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard image="https://picsum.photos/id/31/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard class="md:block hidden" image="https://picsum.photos/id/32/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard class="lg:block hidden" image="https://picsum.photos/id/33/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard class="xl:block hidden" image="https://picsum.photos/id/35/300/300" title="Title here" subTitle="Subtitle here"/>
+    </div>
+  </div>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Recommend podcasts
+    </button>
+    <div class="py-1 5"></div>
+    <div class="flex items-center">
+      <HomeCard image="https://picsum.photos/id/30/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard image="https://picsum.photos/id/31/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard class="md:block hidden" image="https://picsum.photos/id/32/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard class="lg:block hidden" image="https://picsum.photos/id/33/300/300" title="Title here" subTitle="Subtitle here"/>
+      <HomeCard class="xl:block hidden" image="https://picsum.photos/id/35/300/300" title="Title here" subTitle="Subtitle here"/>
+    </div>
+  </div>
 </template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
