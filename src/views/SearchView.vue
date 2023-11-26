13f38ca3c152d59b98343f60e4e57d4e87a15fd6
alyson.ramos <ramos.alysonfarias@gmail.com>
2023-11-26 20:13:54 -0300

    feat: search view

---
+++ src/views/SearchView.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import CategorySelect from '@/components/CategorySelect.vue';
+
+</script>
+
+<template>
+  <div class="p-8">
+    <button
+      type="button"
+      class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+    >
+      Recommend podcasts
+    </button>
+    <div class="py-1 5"></div>
+    <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+        <CategorySelect category="Podcasts" image="https://picsum.photos/id/31/300/300"></CategorySelect>
+        <CategorySelect category="Audiobooks" image="https://picsum.photos/id/20/300/300"></CategorySelect>
+        <CategorySelect category="Made for you" image="https://picsum.photos/id/47/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+        <CategorySelect category="Podcasts" image="https://picsum.photos/id/31/300/300"></CategorySelect>
+        <CategorySelect category="Audiobooks" image="https://picsum.photos/id/20/300/300"></CategorySelect>
+        <CategorySelect category="Made for you" image="https://picsum.photos/id/47/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+        <CategorySelect category="Podcasts" image="https://picsum.photos/id/31/300/300"></CategorySelect>
+        <CategorySelect category="Audiobooks" image="https://picsum.photos/id/20/300/300"></CategorySelect>
+        <CategorySelect category="Made for you" image="https://picsum.photos/id/47/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+        <CategorySelect category="Podcasts" image="https://picsum.photos/id/31/300/300"></CategorySelect>
+        <CategorySelect category="Audiobooks" image="https://picsum.photos/id/20/300/300"></CategorySelect>
+        <CategorySelect category="Made for you" image="https://picsum.photos/id/47/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+        <CategorySelect category="Podcasts" image="https://picsum.photos/id/31/300/300"></CategorySelect>
+        <CategorySelect category="Audiobooks" image="https://picsum.photos/id/20/300/300"></CategorySelect>
+        <CategorySelect category="Made for you" image="https://picsum.photos/id/47/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+        <CategorySelect category="New Releases" image="https://picsum.photos/id/44/300/300"></CategorySelect>
+
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
